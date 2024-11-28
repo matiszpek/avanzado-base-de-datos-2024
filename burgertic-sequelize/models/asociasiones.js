56ea5d6c5ef53533c3c97097299fef91075f949e
Luxcas213 <lgarbate@gmail.com>
2024-11-28 17:21:42 -0300

    kjandksj

--- burgertic-sequelize/models/asociasiones.js
+++ burgertic-sequelize/models/asociasiones.js
@@ -2,24 +2,14 @@ import { Pedido } from "./pedidos.model";
 import { Plato } from "./platos.model";
 import { PlatosXPedidos } from "./PlatosXPedidos";
 import { Usuario } from "./usuarios.model";
-
 import { sequelize } from "../db";
 
-
-
-export const defModelos = async()=>{
-    Pedido.belongsTo(Usuario, { foreignKey: 'UsuarioId' });
-    Usuario.hasMany(Pedido,{ foreignKey: 'UsuarioId' });
-    Pedido.belongsToMany(Plato, {through: PlatosXPedidos,
-        foreignKey: 'id_pedido',
-        otherKey: 'id_plato',
-        as: 'plato',});
-    Plato.belongsToMany(Pedido, {through: PlatosXPedidos,
-        foreignKey: 'id_plato',
-        otherKey: 'id_pedido',
-        as: 'pedido',});
-}  
-sequelize.sync({ force: true }).then(() => {
-    console.log("Tablas sincronizadas");
-});
-
+export const defModelos = async () => {
+    Pedido.belongsToMany(Plato, { through: PlatosXPedidos });
+    Plato.belongsToMany(Pedido, { through: PlatosXPedidos});
+  
+    Usuario.hasMany(Pedido);
+    Pedido.belongsTo(Usuario);
+    console.log("Modelos definidos");
+    await sequelize.sync({ force: false, alter: false });
+  };
